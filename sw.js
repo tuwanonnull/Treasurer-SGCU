c7c453a5d1039b38a67fade60d48c938029720de
treasurersgcu68 <treasurer.sgcu68@gmail.com>
2026-08-28 15:55:31 +0700

    Prevent runtime config from delaying app startup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "20260827-local-date-default-1";
+const CACHE_VERSION = "20260828-runtime-config-hotfix-1";
 const CACHE_PREFIX = "treasurer-sgcu-shell";
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 const APP_SHELL_URLS = [
